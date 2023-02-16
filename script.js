a1f5a06c1d528b5944641305d353094fc1f8998c
alan <alan@iMac-de-alan.local>
2023-02-16 14:34:59 -0600

    Add event listener to be able to select a specific cell on the grid

---
+++ script.js
@@ -0,0 +1,13 @@
+
+    
+
+// tablero
+    let board = document.querySelector('.board');
+    let cells = document.querySelectorAll('.cell');
+
+    cells.forEach(cell => cell.addEventListener('click', function(){
+        console.log(cell.textContent)
+    }))
+ function selectCell(){}
+
+//jugador
