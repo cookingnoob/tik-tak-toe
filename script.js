5b0b8c229038ef054d514770c343f878774264a0
alan <alan@iMac-de-alan.local>
2023-02-17 13:03:27 -0600

    the grid is created with dom elements inside the Gameboard module

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 //un objeto que tiene el tablero
 //Crear array para guardar el tablero, no tengo idea de que se refiere con esto
-// ¿¿sera crear los elementos con tecnicas de dom y guardarlos como objetos?
+//es crear una fabrica para la parrilla del tablero
+//va a tener un valor del numero de casilla, para definir las reglas de ganar/perder/empate
+//va a tener un valor que sera definido cuando el jugador haga click
+
 const Gameboard = (() => {
+    let gameboard = []
+    const board = createElementWithClass ('div', 'board');
+    const one = createElementWithClass ('div', 'cell');
+    const two = createElementWithClass ('div', 'cell');
+    const three = createElementWithClass ('div', 'cell');
+    const four = createElementWithClass ('div', 'cell');
+    const five = createElementWithClass ('div', 'cell');
+    const six = createElementWithClass ('div', 'cell');
+    const seven = createElementWithClass ('div', 'cell');
+    const eight = createElementWithClass ('div', 'cell');
+    const nine = createElementWithClass ('div', 'cell');
+    document.body.appendChild(board);
+    board.append(one, two, three, four, five, six, seven, eight, nine)
 
-    const board = document.querySelector('.board');
-    const cells = document.querySelectorAll('.cell');
-    const click = cells.forEach(cell => chooseCell(cell));
-
-    function chooseCell(cell){
-        cell.addEventListener('click', function(){
-            if(cell.textContent != ''){
-                return
-            }else { 
-            cell.textContent = 'O'
-            }
-        })
-    }
+    function createElementWithClass(elementType, className){
+            let element = document.createElement(elementType);
+            element.classList.add(className);
+            gameboard.push(element);
+            return element;
+        }
 
     return{
-        click
+        // click
+        
     }
 
 })();
+
+//va a tomar los valores de la casilla para las reglas del juego
 
 const displayController = (() => { 
 })();
@@ -30,21 +42,25 @@ const displayController = (() => {
 //son dos botones
 //se elige picando uno 
 const Player = (name) => {
-    const playerSelection = document.querySelector('.playerSelection');
+    //creates buttons
     const choosePlayer = document.createElement('div');
     choosePlayer.classList.add('playerBtn');
     choosePlayer.textContent = name;
+    
+
+
+    const playerSelection = document.querySelector('.playerSelection');
     playerSelection.appendChild(choosePlayer);
 
 
-  const sayName = () => `my name is ${name}`
+    const sayName = () => `my name is ${name}`
+    
   
   return{
     sayName
   }
    
 }
-
 const playerX = Player('X')
 playerX.sayName()
 
@@ -52,11 +68,19 @@ const playerO = Player('O')
 
 
 
-            // const playerX = createElementWithClass('div', 'playerX');
-    // const playerO = createElementWithClass('div', 'playerO')
 
-    // function createElementWithClass(elementType, className){
-    //     let element = document.createElement(elementType);
-    //     element.classList.add(className);
-    //     return element;
+
+
+        // const board = document.querySelector('.board');
+    // const cells = document.querySelectorAll('.cell');
+    // const click = cells.forEach(cell => chooseCell(cell));
+
+    // function chooseCell(cell){
+    //     cell.addEventListener('click', function(){
+    //         if(cell.textContent != ''){
+    //             return
+    //         }else { 
+    //         cell.textContent = 'O'
+    //         }
+    //     })
     // }
