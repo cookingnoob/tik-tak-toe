1df885a303891d4c3b46c5dd8b1f4196ffec1b55
alan <alan@iMac-de-alan.local>
2023-02-17 11:17:58 -0600

    pass the function to choose a checker into the Gameboard module, there is no problem with the scope on the eventListener inside the forEach

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
+//un objeto que tiene el tablero
+//Crear array para guardar el tablero, no tengo idea de que se refiere con esto
+// ¿¿sera crear los elementos con tecnicas de dom y guardarlos como objetos?
 const Gameboard = (() => {
-    let board = document.querySelector('.board');
-    const click = board.addEventListener('click',prueba);
-    function prueba(){
-        console.log('hola')
+
+    const board = document.querySelector('.board');
+    const cells = document.querySelectorAll('.cell');
+    const click = cells.forEach(cell => chooseCell(cell));
+
+    function chooseCell(cell){
+        cell.addEventListener('click', function(){
+            if(cell.textContent != ''){
+                return
+            }else { 
+            cell.textContent = 'O'
+            }
+        })
     }
-    return {
+
+    return{
         click
     }
+
 })();
 
-    
-
-// esto permite que puedas seleccionar un cuadro
 const displayController = (() => { 
-    let board = document.querySelector('.board');
-    let cells = document.querySelectorAll('.cell');
-            //¿¿Como compartir las variables dentro de la funcion?
-    cells.forEach(cell => cell.addEventListener('click', function chooseCell(){
-        if(cell.textContent != ''){
-            return
-        }else { 
-        cell.textContent = 'O'
-        }
-    }))    
 })();
+
+const Player = (checker) => {
+    const chooseChecker = document.getElementsByClassName('.chooseChecker');
+    const playerX = createElementWithClass('div', 'playerX');
+    const playerO = createElementWithClass('div', 'playerO')
+
+    function createElementWithClass(elementType, className){
+        let element = document.createElement(elementType);
+        element.classList.add(className);
+        return element;
+    }
+}
 
 //jugador
+        //tiene un X o O 
+        //cada click pasa ese elemento a la casilla
+        //no puede haber dos iguales
