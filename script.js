e7ed8d29d10a2aece660848c12f1e63e866235ef
alan <alan@iMac-de-alan.local>
2023-02-18 14:07:35 -0600

    player passes its value to gameBoard

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-//un objeto que tiene el tablero
-//Crear array para guardar el tablero, no tengo idea de que se refiere con esto
-//es crear una fabrica para la parrilla del tablero
-//va a tener un valor del numero de casilla, para definir las reglas de ganar/perder/empate
-//va a tener un valor que sera definido cuando el jugador haga click
+//crear objeto cell - tieneun indicador numerico (creado por el loop) y una variable sin definir
+//usar el loop para crear 9 casillas
+//manda los objetos a un array
+//acepta un valor que es dado por el player y va a la variable sin definir
+//renderea en pantalla ese valor
+//el jugador inicia y despues va la computadora (o un humano)
+//la computadora hace un loop sobre el array para escoger un numero randon
+//X va primero y luego va O
+//saca el valor de las casillas
+//exportar el valor de las casillas para que un externo defina el resultado del match
 
 const Gameboard = (() => {
-    let gameboard = []
+    //renders board
+    let gameBoard = []
     const board = createElementWithClass ('div', 'board');
-    const one = createElementWithClass ('div', 'cell');
-    const two = createElementWithClass ('div', 'cell');
-    const three = createElementWithClass ('div', 'cell');
-    const four = createElementWithClass ('div', 'cell');
-    const five = createElementWithClass ('div', 'cell');
-    const six = createElementWithClass ('div', 'cell');
-    const seven = createElementWithClass ('div', 'cell');
-    const eight = createElementWithClass ('div', 'cell');
-    const nine = createElementWithClass ('div', 'cell');
     document.body.appendChild(board);
-    board.append(one, two, three, four, five, six, seven, eight, nine)
 
+    //creates enough cells for a 3x3 board (9 cells)
+    for (let i = 0; i <=8; i++){
+        let cell = createElementWithClass ('div', 'cell');
+        board.append(cell);
+        gameBoard.push(cell)
+    }
+    
+    //
+    const cells = document.querySelectorAll('.cell');
+    const click = cells.forEach(cell => chooseCell(cell));
+
+    function chooseCell(cell){
+        cell.addEventListener('click', function(){
+            if(cell.textContent != ''){
+                return
+            } else { 
+            cell.textContent = playerX.sayName()
+            }
+        })
+    }
+    //ya crea un elemento con clase para el DOM
+    //falta que agregue elementos de objeto como un indicador y un valor sin definir
     function createElementWithClass(elementType, className){
-            let element = document.createElement(elementType);
-            element.classList.add(className);
-            gameboard.push(element);
-            return element;
-        }
+        let element = document.createElement(elementType);
+        element.classList.add(className);
+        return element;
+    }
 
     return{
-        // click
-        
+        click 
     }
 
 })();
@@ -40,47 +56,38 @@ const displayController = (() => {
 
 
 //son dos botones
-//se elige picando uno 
+//cada uno tiene un valor diferente
+//se elige que valor vas a exportar al tablero picando su respectivo boton
+//
+
 const Player = (name) => {
     //creates buttons
     const choosePlayer = document.createElement('div');
     choosePlayer.classList.add('playerBtn');
     choosePlayer.textContent = name;
-    
-
 
     const playerSelection = document.querySelector('.playerSelection');
     playerSelection.appendChild(choosePlayer);
 
-
-    const sayName = () => `my name is ${name}`
-    
+//exporta el valor del jugador
+    const sayName = () => ` ${name}`
+  
   
   return{
     sayName
+    
   }
-   
+
 }
 const playerX = Player('X')
 playerX.sayName()
 
 const playerO = Player('O')
-
+playerO.sayName()
 
 
 
 
 
         // const board = document.querySelector('.board');
-    // const cells = document.querySelectorAll('.cell');
-    // const click = cells.forEach(cell => chooseCell(cell));
-
-    // function chooseCell(cell){
-    //     cell.addEventListener('click', function(){
-    //         if(cell.textContent != ''){
-    //             return
-    //         }else { 
-    //         cell.textContent = 'O'
-    //         }
-    //     })
-    // }
+    
