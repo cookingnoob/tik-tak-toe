b8ad51187743583bc137e305f4fcfde46ad2ca7d
alan <alan@iMac-de-alan.local>
2023-02-22 11:34:40 -0600

    uncomment function to add the checker as textcontent to the cell

--- script.js
+++ script.js
@@ -2,40 +2,47 @@
 const Gameboard = (() => {
     let gameBoard = [];
     
-    //renders board
+    //Renders board
     const board = document.createElement('div');
     board.classList.add('board')
     document.body.appendChild(board);
 
-   
+    //cell factory
     const Cell = (identifier) => {
         const cellElement = document.createElement('div');
         cellElement.classList.add('cell');
         board.appendChild(cellElement);
         return{cellElement, identifier, value : ``}
     }
-
+    //creates cells with dynamic naming
     let cellsLoop = {}
     for (let i = 0; i < 9; i++){
         cellsLoop[`cell${i}`] =  Cell(i);
         gameBoard.push(cellsLoop[`cell${i}`]);
     }
-   
+    
+    //select a cell to add the player checker as text content
+    const cells = document.querySelectorAll('.cell');
+    const clickCell = cells.forEach(cell => addText(cell));
+    function addText(cell){
+         cell.addEventListener('click', function(){
+             if(cell.textContent != ''){
+                 return
+             } else { 
+             cell.textContent = playerValue; 
+             }
+         })
+     }
+    //add the value of the checker to the value of the cell object value : X
     gameBoard.forEach(cell => {
         cell.cellElement.addEventListener('click', function(){
-            cell.value = playerValue
-            console.log(cell);
-            console.log(gameBoard)
+            cell.value = playerValue;
+            console.log(gameBoard[0].value + gameBoard[1].value + gameBoard[2].value);
+            
         })
     });
     
-
-   
-
-    return{
-       
-      
-    }
+    return{}
 
 })();
 
@@ -69,24 +76,3 @@ playerX.click()
 
 const playerO = Player('O')
 playerO.click()
-
-
-
-
-
-        // const board = document.querySelector('.board');
-     //click a cell to pass the palayer value
-    //  const cells = document.querySelectorAll('.cell');
-    //  const clickCell = cells.forEach(cell => chooseCell(cell));
- 
-    //  function chooseCell(cell){
-    //      cell.addEventListener('click', function(){
-    //          if(cell.textContent != ''){
-    //              return
-    //          } else { 
-    //          cell.value = playerValue;
-    //          cell.textContent = playerValue;
-             
-    //          }
-    //      })
-    //  }
